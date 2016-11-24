18f532bb268e6d3394235913bbc136761707ff0d
AvengerPenguin <github@rossfenning.co.uk>
2016-11-24 15:37:53 +0000

    Use env vars for details

--- test/test_collection.py
+++ test/test_collection.py
@@ -1,12 +1,13 @@
 from __future__ import unicode_literals, print_function
+import os
 import imapclient
 from backports import ssl
-
 from fixture_proxy import ImapProxy
 
-HOST = 'mail.rossfenning.co.uk'
-USERNAME = 'post@rossfenning.co.uk'
-PASSWORD = 'c4ffr3y5'
+
+HOST = os.getenv('MAIL_HOST')
+USERNAME = os.getenv('USER')
+PASSWORD = os.getenv('PASSWORD')
 
 
 def test():
@@ -18,7 +19,9 @@ def test():
     # don't check if the certificate is trusted by a certificate authority
     context.verify_mode = ssl.CERT_NONE
 
-    server = ImapProxy(imapclient.IMAPClient(HOST, use_uid=True, ssl=True, ssl_context=context))
+    server = ImapProxy(
+        imapclient.IMAPClient(HOST, port=8993, use_uid=True, ssl=True, ssl_context=context)
+    )
     server.login(USERNAME, PASSWORD)
 
     stuff = server.folder_status('INBOX', ('MESSAGES'))[b'MESSAGES']
