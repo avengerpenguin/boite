9d1300c88c988318d84cd4fc7c39ef7e80af520e
AvengerPenguin <github@rossfenning.co.uk>
2016-12-05 21:53:10 +0000

    Capable of matching email bodies

--- boite/__init__.py
+++ boite/__init__.py
@@ -1,14 +1,22 @@
 from __future__ import unicode_literals, print_function
 import email
 import os
-from email.parser import BytesParser
+import sys
 import imapclient
 from backports import ssl
 from datetime import date
+import logging
+
 
 HOST = os.getenv('MAIL_HOST')
 USERNAME = os.getenv('USER')
 PASSWORD = os.getenv('PASSWORD')
+
+
+LOG = logging.getLogger()
+LOG.setLevel(logging.INFO)
+handler = logging.StreamHandler(sys.stderr)
+LOG.addHandler(handler)
 
 
 def make_server():
@@ -20,25 +28,130 @@ def make_server():
     # don't check if the certificate is trusted by a certificate authority
     context.verify_mode = ssl.CERT_NONE
 
+    LOG.info('Logging in to {host}...'.format(host=HOST))
     server = imapclient.IMAPClient(HOST, use_uid=True, ssl=True, ssl_context=context)
     server.login(USERNAME, PASSWORD)
     return server
 
 
+def check_match(message, header, pattern):
+
+    if header == 'Body':
+        value = ''
+
+        for m in (part for part in message.walk() if not part.is_multipart()):
+            try:
+                value = m.get_payload(decode=True)
+                break
+            except UnicodeEncodeError:
+                print('UNICODE ERROR')
+                m.set_charset('utf-8')
+                value = m.get_payload(decode=True)
+                break
+            except KeyError:
+                value = ''
+                continue
+
+        message = m
+    else:
+        value = message[header]
+
+    #print('Using value: ' + repr(value))
+
+    if type(value) == bytes:
+        charset = message.get_charset()
+        if charset:
+            charset = charset.input_charset
+        else:
+            content_type = (message['Content-Type'] or '').lower()
+            if 'iso-8859-1' in content_type:
+                charset = 'iso-8859-1'
+            elif 'windows-1252' in content_type:
+                charset = 'windows-1252'
+            else:
+                charset = 'utf-8'
+        value = value.decode(charset, errors='replace')
+
+    try:
+        return pattern.match(value or '')
+    except AttributeError:
+        return value == pattern
+
+
 def archive_stale(matchers, age):
     cutoff = date.today() - age
     server = make_server()
+
+    LOG.info('Opening INBOX')
     server.select_folder('INBOX')
+
+    LOG.info('Looking at messages from before {cutoff}'.format(cutoff=cutoff))
     message_ids = server.search(criteria=['BEFORE', cutoff])
     messages = server.fetch(message_ids, ['INTERNALDATE', 'RFC822', 'UID'])
+
     for uid, raw_message in messages.items():
+        print('.', file=sys.stderr, end='')
+        sys.stderr.flush()
+        if b'RFC822' not in raw_message:
+            LOG.error('Weird message: ' + str(raw_message))
+            continue
         message = email.message_from_bytes(raw_message[b'RFC822'])
+
         for matcher in matchers:
-            matches = sum([1 for header, pattern in matcher.items() if message[header] == pattern])
+            matches = sum([1 for header, pattern in matcher.items() if check_match(message, header, pattern)])
+
             if matches == len(matcher):
+                print('.', file=sys.stderr)
+                LOG.info('Archiving message with subject "{subject}" as '
+                         'it matches {matcher}'.format(
+                    subject=message['Subject'], matcher=matcher))
+
                 server.copy([uid], 'archive')
                 server.delete_messages([uid])
                 server.expunge()
+                continue
+
+    print('.', file=sys.stderr)
+
+
+def mark_spam(matchers):
+    server = make_server()
+
+    LOG.info('Opening INBOX')
+    server.select_folder('INBOX')
+
+    LOG.info('Scanning all messages for spam...')
+
+    message_ids = server.search()
+    messages = server.fetch(message_ids, ['INTERNALDATE', 'RFC822', 'UID'])
+
+    for uid, raw_message in messages.items():
+        print('.', file=sys.stderr, end='')
+        sys.stderr.flush()
+        if b'RFC822' not in raw_message:
+            LOG.error('Weird message: ' + str(raw_message))
+            continue
+        message = email.message_from_bytes(raw_message[b'RFC822'] or b'')
+
+        for matcher in matchers:
+            try:
+                matches = sum([1 for header, pattern in matcher.items() if check_match(message, header, pattern)])
+            except Exception as e:
+                print('Error {e} processing message: {message}'.format(e=e, message=repr(message.as_bytes())))
+                sys.exit(1)
+
+            if matches == len(matcher):
+                print('.', file=sys.stderr)
+                LOG.info('Marking as spam message with subject "{subject}" as it '
+                         'matches {matcher}'.format(
+                    subject=message['Subject'], matcher=matcher))
+
+                server.copy([uid], 'spam')
+                server.delete_messages([uid])
+                server.expunge()
+                break
+
+    print('.', file=sys.stderr)
 
 
 class Boite(object):
