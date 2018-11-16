780cbcc240f6707f0d8947bd9fa1547ef9006bd3
AvengerPenguin <github@rossfenning.co.uk>
2018-11-16 20:27:18 +0000

    Much better handling of variation across email data structures

--- boite/__init__.py
+++ boite/__init__.py
@@ -19,6 +19,49 @@ handler = logging.StreamHandler(sys.stderr)
 LOG.addHandler(handler)
 
 
+def query_yes_no(question, default="no"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
+
+def create_default_context():
+    return imapclient.create_default_context()
+
+
+def IMAP(host=None, port=None, user=None, password=None, use_uid=True, ssl=True, ssl_context=None):
+    server = imapclient.IMAPClient(host, port=port, use_uid=use_uid, ssl=ssl, ssl_context=ssl_context)
+    server.login(user, password)
+    return server
+
+
 def make_server():
     context = imapclient.create_default_context()
 
@@ -56,8 +99,6 @@ def check_match(message, header, pattern):
     else:
         value = message[header]
 
-    #print('Using value: ' + repr(value))
-
     if type(value) == bytes:
         charset = message.get_charset()
         if charset:
@@ -73,14 +114,16 @@ def check_match(message, header, pattern):
         value = value.decode(charset, errors='replace')
 
     try:
-        return pattern.match(value or '')
+        return pattern.match(str(value) or '')
     except AttributeError:
         return value == pattern
 
 
-def archive_stale(matchers, age):
+def archive_stale(server, matchers, age, folder=None):
     cutoff = date.today() - age
-    server = make_server()
+
+    if not folder:
+        folder = 'archive'
 
     LOG.info('Opening INBOX')
     server.select_folder('INBOX')
@@ -92,10 +135,15 @@ def archive_stale(matchers, age):
     for uid, raw_message in messages.items():
         print('.', file=sys.stderr, end='')
         sys.stderr.flush()
-        if b'RFC822' not in raw_message:
+        if b'RFC822' not in raw_message and b'BODY[NULL]' not in raw_message:
             LOG.error('Weird message: ' + str(raw_message))
             continue
-        message = email.message_from_bytes(raw_message[b'RFC822'])
+        if b'RFC822' in raw_message:
+            message = email.message_from_bytes(raw_message[b'RFC822'])
+        else:
+            message = email.message_from_bytes(raw_message[b'BODY[NULL]'])
+
+        #print(message.as_string())
 
         for matcher in matchers:
             matches = sum([1 for header, pattern in matcher.items() if check_match(message, header, pattern)])
@@ -106,17 +154,15 @@ def archive_stale(matchers, age):
                          'it matches {matcher}'.format(
                     subject=message['Subject'], matcher=matcher))
 
-                server.copy([uid], 'archive')
+                server.copy([uid], folder)
                 server.delete_messages([uid])
                 server.expunge()
-                continue
+                break
 
     print('.', file=sys.stderr)
 
 
-def mark_spam(matchers):
-    server = make_server()
-
+def mark_spam(server, matchers):
     LOG.info('Opening INBOX')
     server.select_folder('INBOX')
 
@@ -153,6 +199,90 @@ def mark_spam(matchers):
 
     print('.', file=sys.stderr)
 
+
+class Message(object):
+    def __init__(self, server, uid, message):
+        self.server = server
+        self.uid = uid
+        self.message = message
+
+    @property
+    def headers(self):
+        return dict(self.message)
+
+    @property
+    def body(self):
+        value = ''
+
+        for m in (part for part in self.message.walk() if not part.is_multipart()):
+            try:
+                value = m.get_payload(decode=True)
+                break
+            except UnicodeEncodeError:
+                print('UNICODE ERROR')
+                m.set_charset('utf-8')
+                value = m.get_payload(decode=True)
+                break
+            except KeyError:
+                value = ''
+                continue
+
+        return value
+
+    def archive(self, folder=None):
+        LOG.info('Archiving {}'.format(self.uid))
+        self.server.copy([self.uid], folder or 'archive')
+        self.server.delete_messages([self.uid])
+        self.server.expunge()
+
+    def mark_spam(self):
+        self.server.copy([self.uid], 'spam')
+        self.server.delete_messages([self.uid])
+        self.server.expunge()
+
+
+def stream(server, matchers, age=None):
+    LOG.info('Opening INBOX')
+    server.select_folder('INBOX')
+
+    LOG.info('Scanning all messages for custom processing...')
+
+    if age:
+        cutoff = date.today() - age
+        LOG.info('Looking at messages from before {cutoff}'.format(cutoff=cutoff))
+        message_ids = server.search(criteria=['BEFORE', cutoff])
+        messages = server.fetch(message_ids, ['INTERNALDATE', 'RFC822', 'UID'])
+    else:
+        message_ids = server.search()
+        messages = server.fetch(message_ids, ['INTERNALDATE', 'RFC822', 'UID'])
+
+    for uid, raw_message in messages.items():
+        print('.', file=sys.stderr, end='')
+        sys.stderr.flush()
+
+        if b'RFC822' not in raw_message and b'BODY[NULL]' not in raw_message:
+            LOG.error('Weird message: ' + str(raw_message))
+            LOG.error(raw_message.keys())
+            continue
+        message = email.message_from_bytes(raw_message.get(b'RFC822', raw_message.get(b'BODY[NULL]', b'')))
+
+        for matcher in matchers:
+            try:
+                matches = sum([1 for header, pattern in matcher.items() if check_match(message, header, pattern)])
+            except Exception as e:
+                print('Error {e} processing message: {message}'.format(e=e, message=repr(message.as_bytes())))
+                sys.exit(1)
+
+            if matches == len(matcher):
+                print('.', file=sys.stderr)
+                # LOG.info('Doing custom action to message with subject '
+                #          '"{subject}" as it matches {matcher}'.format(
+                #     subject=message['Subject'], matcher=matcher))
+
+                yield Message(server, uid, message)
+                break
+
+    print('.', file=sys.stderr)
 
 class Boite(object):
     def __init__(self, server, inbox='INBOX'):
